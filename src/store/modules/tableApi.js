98ac7c369e0abd8a2b74b7d8837ddc48cb51d09d
ann <ann110149@gmail.com>
2021-11-15 22:33:47 +0800

    update

--- src/store/modules/tableApi.js
+++ src/store/modules/tableApi.js
@@ -1,20 +1,51 @@
 import api from '@/service/api'
-const tableModules ={
+const tableModule = {
   state:{
-
+    res:[]
   },
 
   getters:{
-
+    getResponse(state){
+      return state.res
+    }
   },
   
   mutations:{
-
+    SET_RES(state,payload){
+      state.res = payload
+      console.log('Mutation SET_RES: ', payload)
+    }
   },
 
   actions:{
+    async GET_API(context, payload){
+      const url='http://localhost:3000/tableApi'
+      const res=await api.get(url)
+      payload=res
+      context.commit('SET_RES', payload)
+      console.log('Actions GET_API: ', res, payload)
+      console.log('Actions GET_API context: ', context)
+    },
+    async POST_API(context, payload){
+      const url='http://localhost:3000/tableApi'
+      await api.post(url, payload)
+      context.dispatch('GET_API')
+      console.log('Actions POST_API: ', payload)
+    },
+    async PUT_API(context, payload){
+      const url=`http://localhost:3000/tableApi/${payload.id}`
+      await api.put(url,payload)
+      context.dispatch('GET_API')
+      console.log('Actions PUT_API: ', payload)
+    },
+    async DELE_API(context, payload){
+      const url=`http://localhost:3000/tableApi/${payload}`
+      await api.delete(url)
+      context.dispatch('GET_API')
+      console.log('Actions DELE_API: ', payload)
 
+    }
   }
 }
 
-export default tableModules
+export default tableModule
--- src/views/Vuex.vue
+++ src/views/Vuex.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="row vertical" data-inset="1rem">
-    
+    <div class="row horizontal v_center" data-space-bottom="0.5rem">
+      <el-button type="primary">新增商品</el-button>
+    </div>
+    <el-table :data="getResponse" style="width:100%">
+        <el-table-column prop="name" label="商品名稱"/>
+        <el-table-column prop="price" label="售價"/>
+        <el-table-column prop="count" label="剩餘數量"/>
+        <el-table-column label="操作">
+        </el-table-column>
+      </el-table>
   </div>
 </template>
 
 <script>
 import {mapState, mapGetters, mapMutations, mapActions} from "vuex"
 export default {
-  name:"vuex",
+  name:'vuex',
   data () {
     return {
-      tableData:[],
       showDialog:false,
       formData:{
         id:'',
@@ -18,17 +26,26 @@ export default {
         price:'',
         count:'',
         img:''
-      }
+      },
+      submitStatus: ''
     }
   },
   computed:{
-
+    ...mapGetters(['getResponse'])
   },
   methods:{
+    ...mapActions(['GET_API', 'POST_API', 'PUT_API', 'DELE_API']),
+    async callApi () {
+      this.GET_API()
+    },
     
+  
+  
+  
   },
+
   created () {
-    
+    this.callApi()
   },
   mounted(){
 
