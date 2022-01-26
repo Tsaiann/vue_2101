1c31c9a2cb21378f37b4e9d6325f5b92f0fdc77e
ann <ann110149@gmail.com>
2022-01-26 22:13:15 +0800

    update i18n

--- src/views/I18n_change.vue
+++ src/views/I18n_change.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="row vertical" data-inset="1rem">
-    </div>  
+  <div class="row horizontal" data-inset="1rem">
+    <el-button>{{ $t('defalt') }}</el-button>
+    <el-button type="primary">{{ $t('primary') }}</el-button>
+    <el-button type="success">{{ $t('success') }}</el-button>
+    <el-button type="info">{{ $t('info') }}</el-button>
+    <el-button type="warning">{{ $t('warning') }}</el-button>
+    <el-button type="danger">{{ $t('danger') }}</el-button>
+  </div>  
 </template>
 <script>
 export default {
